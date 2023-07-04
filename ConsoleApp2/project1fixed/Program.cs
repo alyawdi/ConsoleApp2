7fc00f62f51178da23226404842c8f0081252660
10121668@mu.edu.lb <10121668@mu.edu.lb>
2023-07-04 20:29:28 +0300

    updating the methods linq methods are used variables are given by the user

--- ConsoleApp2/project1fixed/Program.cs
+++ ConsoleApp2/project1fixed/Program.cs
@@ -1,84 +1,140 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    class Target
-    {   //attributes
-        public string Name;
+class Target
+{
+    public string Name;
 
-        //construtor
-        public Target(string name)
-        {
-            Name = name;
-        }
-    }
-
-    class Program
+    public Target(string name)
     {
-        static List<Target> targetList = new List<Target>();
+        Name = name;
+    }
+}
 
+class Program
+{
+    static List<Target> targetList = new List<Target>();
 
-        //add method
-        static void AddTarget(Target target)
+    // Add method
+    static void AddTarget(Target target)
+    {
+        targetList.Add(target);
+        Console.WriteLine("Target added successfully!");
+    }
+
+    // Fetch method
+    static Target FetchTarget(string searchName)
+    {
+        Target target = targetList.FirstOrDefault(t => t.Name.Equals(searchName, StringComparison.OrdinalIgnoreCase));
+
+        if (target != null)
         {
-            targetList.Add(target);
-            Console.WriteLine("Target added successfully!");
+            return target;
         }
 
-        //fetch method
-        static Target FetchTarget(string searchName)
+        Console.WriteLine("No matching target found.");
+        return null;
+    }
+
+    // Update method
+    static void UpdateTarget(string oldName, string newName)
+    {
+        Target target = targetList.FirstOrDefault(t => t.Name.Equals(oldName, StringComparison.OrdinalIgnoreCase));
+
+        if (target != null)
         {
-            for (int i = 0; i < targetList.Count; i++)
-            {
-                if (targetList[i].Name.Equals(searchName, StringComparison.OrdinalIgnoreCase))
-                {
-                    return targetList[i]; // i took fetch as should i return the target not displayed it 
-                }
-            }
-            Console.WriteLine("no matching answer found");
-            return null;
+            target.Name = newName;
+            Console.WriteLine("Target updated successfully!");
         }
-
-        static void UpdateTarget(string oldname, string newName)
+        else
         {
-            for (int i = 0; i < targetList.Count; i++)
-            {
-                if (targetList[i].Name == oldname)
-                {
-                    targetList[i].Name = newName;
-                }
-            }
-        }
-
-        //remove method
-        static void RemoveTarget(string name)
-        {
-            for (int i = 0; i < targetList.Count; i++)
-            {
-                if (targetList[i].Name == name)
-                {
-                    targetList.Remove(targetList[i]);
-
-                }
-            }
-        }
-        static void Main(string[] args)
-        {
-            AddTarget(new Target("hussein"));
-            AddTarget(new Target("ali"));
-            AddTarget(new Target("leo"));
-
-            FetchTarget("mhmd");
-            UpdateTarget("ali", "mhmd");
-            FetchTarget("mhmd");
-
-            RemoveTarget("hussein");
-            FetchTarget("ali");
-
-            foreach (Target target in targetList)
-            {
-                Console.WriteLine(target.Name);
-
-            }
-
-
+            Console.WriteLine("No matching target found.");
         }
     }
+
+    // Remove method
+    static void RemoveTarget(string name)
+    {
+        Target target = targetList.FirstOrDefault(t => t.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+        if (target != null)
+        {
+            targetList.Remove(target);
+            Console.WriteLine("Target removed successfully!");
+        }
+        else
+        {
+            Console.WriteLine("No matching target found.");
+        }
+    }
+
+    // Print all targets method
+    static void PrintAllTargets()
+    {
+        Console.WriteLine("\nList of targets:");
+        foreach (Target target in targetList)
+        {
+            Console.WriteLine(target.Name);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Target Manager!");
+
+        while (true)
+        {
+            Console.WriteLine("\nChoose an option:");
+            Console.WriteLine("1. Add a target");
+            Console.WriteLine("2. Fetch a target");
+            Console.WriteLine("3. Update a target");
+            Console.WriteLine("4. Remove a target");
+            Console.WriteLine("5. Print all targets");
+            Console.WriteLine("6. Exit");
+
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter target name: ");
+                    string targetName = Console.ReadLine();
+                    AddTarget(new Target(targetName));
+                    break;
+                case "2":
+                    Console.Write("Enter target name to fetch: ");
+                    string fetchName = Console.ReadLine();
+                    Target fetchedTarget = FetchTarget(fetchName);
+                    if (fetchedTarget != null)
+                    {
+                        Console.WriteLine("Fetched target: " + fetchedTarget.Name);
+                    }
+                    break;
+                case "3":
+                    Console.Write("Enter target name to update: ");
+                    string oldName = Console.ReadLine();
+                    Console.Write("Enter new target name: ");
+                    string newName = Console.ReadLine();
+                    UpdateTarget(oldName, newName);
+                    break;
+                case "4":
+                    Console.Write("Enter target name to remove: ");
+                    string removeName = Console.ReadLine();
+                    RemoveTarget(removeName);
+                    break;
+                case "5":
+                    PrintAllTargets();
+                    break;
+                case "6":
+                    Console.WriteLine("Exiting the program...");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
